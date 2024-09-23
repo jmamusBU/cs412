73c4715389313147bffaba9114062465e8b446c7
jmamus <jmamus@bu.edu>
2024-09-23 12:02:26 -0400

    add base_dir

--- cs412/settings.py
+++ cs412/settings.py
@@ -117,6 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+# sus line
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 STATIC_URL = 'static/'
 
 import os 
