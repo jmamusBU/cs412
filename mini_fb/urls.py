79396322e0bfa8b18ef6adc2bb0e824c7ff41be0
jmamusBU <jmamus@bu.edu>
2024-10-04 00:31:57 -0400

    testing deployment on heroku

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -3,9 +3,9 @@
 
 from django.urls import path
 from django.conf import settings
-from .views import ShowAllView
+from .views import ShowAllProfilesView
 
 # all of the URLs that are part of this app
 urlpatterns = [
-    path('', ShowAllView.as_view(), name='show_all'),
+    path('', ShowAllProfilesView.as_view(), name='show_all_profiles'),
 ]
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -5,8 +5,8 @@ from django.views.generic import ListView
 from .models import Profile
 # Create your views here.
 
-class ShowAllView(ListView):
+class ShowAllProfilesView(ListView):
     model = Profile # the model to display
-    template_name = 'mini_fb/show_all.html'
+    template_name = 'mini_fb/show_all_profiles.html'
     context_object_name = 'profiles' # context variable to use in the template
     
